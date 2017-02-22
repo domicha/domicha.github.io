d4b07aff97e4d34c7a5debd62198807952b895da
domicha <ddomichael@gmail.com>
2017-02-22 17:32:37 -0500

    Server Update

--- server.js
+++ server.js
@@ -2,20 +2,23 @@
 const express = require('express');
 const app = express();
 
-// // NEW CODE
-// // when the server receives a GET request to '/'
-// app.get('/', (req, res) => {
-// 	// send some text back as a response
-// 	res.send('Express is running!');
-// });
-// // END OF NEW CODE
+// app.set('view engine', 'ejs')
 
 // NEW CODE
-app.use(express.static(__dirname + '/build'));
+// when the server receives a GET request to '/'
+// app.get('/', (req, res) => {
+	// send some text back as a response
+	//res.send('Express is running!');
+    //res.render('public')
+// });
+// END OF NEW CODE
+
+// NEW CODE
+app.use(express.static(__dirname + '/public'));
 // END OF NEW CODE
 
 // start Express on port 8080
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT,() => {
 	console.log("Listening on Port:%s",PORT)
